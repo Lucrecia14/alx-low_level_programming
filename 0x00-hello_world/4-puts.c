0bb496dfcaeeedcbcf5b0306c1d891f8634b8821
Lucrecia14 <joyce.idowu90@gmail.com>
2023-03-22 05:56:53 -0700

    update hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+
+
 /**
  * main - A program that prints a line with puts function
- * Return: 0
+ * Return: Always 0.
  */
-int main(void)
+
+
+int main()
+
 {
-puts("\"Programming is like building a mul"ilingual puzzle");
-return (0);
+	puts("\"Programming is like building a mul"ilingual puzzle");
+	return (0);
+
 }
