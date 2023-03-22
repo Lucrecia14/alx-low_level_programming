6ffc9908f6e690b04a8b9e1759bc663632c25c39
Lucrecia14 <joyce.idowu90@gmail.com>
2023-03-22 06:37:34 -0700

    corrections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints "with proper grammar, but the otcome is a piece of art," followed by a new line
+ * main - A program that prints "with proper grammar, but the otcome is a piece of art,"
+ * followed by a new line
  * Return: Always 0.
  */
 
