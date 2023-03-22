9b1e3c8d7a6e72e58dfdf2d93d329bf33cc0ade0
Lucrecia14 <joyce.idowu90@gmail.com>
2023-03-22 15:16:32 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A program that pints the size of various
+ * types on the computer it is completed and run on
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return: (0);
+}
+
